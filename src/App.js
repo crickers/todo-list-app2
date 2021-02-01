18b8881b99537809ab5a1b3d21c097ec35284430
Cricky <crickmonster@gmail.com>
2021-01-31 23:54:30 -0500

    working on nav bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import logo from "./logo.svg";
-import { Button } from "antd";
+//import { Button } from "antd";
 import "./App.css";
+import Nav from "./nav";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
     </div>
   );
 }
 
 export default App;
+
+//<div className="App">
+//<header className="App-header">
+// <img src={logo} className="App-logo" alt="logo" />
+//  <p>
+//    Edit <code>src/App.js</code> and save to reload.
+// </p>
+//  <a
+//    className="App-link"
+//    href="https://reactjs.org"
+//    target="_blank"
+//    rel="noopener noreferrer">
+
+//    Learn React
+//  </a>
+//</header>
+//</div>
---
+++ src/nav.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from "react";
+
+
+
+function Nav(props) {
+  return (
+<div>
+  <div className={'p-5 text-center bg-blue-600'}>
+      <ul>
+          <li className={'inline p-5'}>About</li>
+          <li className={'inline p-5'}>Todos</li>
+          <li className={'inline p-5'}>Contact</li>
+      </ul>>
+    </ul>
+ </div>
+ </div>
+  );
+}
+
+export default Nav;
